30d572807b5a3642d0a68b86c531a94735657e40
mwdeal1 <mwdeal1@gmail.com>
2017-03-11 09:10:33 -0500

    Finish server.js

--- server.js
+++ server.js
@@ -9,3 +9,12 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.text());
 app.use(bodyParser.json({ type: 'application/vnd.api+json' }));
+
+// route config
+require('./app/routing/apiRoutes')(app);
+require('./app/routing/htmlRoutes')(app);
+
+// start server
+app.listen(PORT, function() {
+    console.log('listening on ' + PORT);
+});
